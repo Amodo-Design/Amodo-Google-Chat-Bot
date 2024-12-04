dc1d5bd4dee8e23d38f7d4ca6cc353656032f787
Amanda Matthes <amanda.matthes@gmail.com>
2024-12-04 17:37:46 +0000

    added slash command support

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import json
-import time
 import logging
+
+import datetime
+import time
+
+import argparse
+
+from pprint import pprint
+
 from google.apps import chat_v1 as google_chat
 from google.cloud import pubsub_v1
 from google.oauth2.service_account import Credentials
 
-from pprint import pprint
+from config_bot import SUBSCRIPTION_PATH
 
-from config import SUBSCRIPTION_PATH
+from chat_bot_tools import send_message
+from summary_tools import breakdown_for_user
+
 
 SCOPES          = ["https://www.googleapis.com/auth/chat.bot"]
 CREDENTIALS     = Credentials.from_service_account_file(
@@ -23,6 +32,10 @@ subscriber = pubsub_v1.SubscriberClient(
     credentials     = CREDENTIALS
 )
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--email', help='email address of the user you want a summary for')
+parser.add_argument('-w', '--week', help='week you want a summary for')
+
 def run_app():
     """
     listen for events and respond to them
@@ -33,7 +46,7 @@ def run_app():
             event = json.loads(message.data)
             print()
             pprint(event)
-            reply(event)
+            react(event)
             message.ack()
         except Exception as e:
             logging.error('error:', e)
@@ -48,38 +61,79 @@ def run_app():
         time.sleep(60)
 
 
-def reply(event):
+def react(event):
     """
     appropriately respond to an event
     """
-    response = None
     # get details about event
     space_name = event['space']['name']
     event_type = event['type']
+
+    sender_email = event['message']['sender']['email']
+
 
     # check if the event removed us from the space
     if event['type'] == 'REMOVED_FROM_SPACE':
         logging.info('app removed rom space %s', space_name)
         return
-    # check if the event was adding us to the space
+    # check if the event added us to the space
     elif event_type == 'ADDED_TO_SPACE':
-        response = google_chat.CreateMessageRequest(
-            parent = space_name,
-            message = {
-            'text': 'Thank you for adding me! Remember that I can only see direct messages.'
-            }
+        send_message(
+            space_name = space_name,
+            message = 'Thank you for adding me! Remember that I can only see direct messages.'
         )
-    # check if the event was a message
-    elif event_type in ['ADDED_TO_SPACE', 'MESSAGE']:
-        response = google_chat.CreateMessageRequest(
-            parent = space_name,
-            message = {
-            'text': 'You said: `' + event['message']['text'] + '`'
-            }
-        )
+        return
+    # check for slash command
+    elif "slashCommand" in event['message']:
+        command_id      = int(event['message']['slashCommand']['commandId'])
 
-    if response is not None:
-        chat.create_message(response)
+        # get arguments
+        if "argumentText" in event['message']:
+            argument_text   = event['message']['argumentText']
+        else:
+            argument_text = ''
+
+        arguments = parser.parse_args(argument_text.split())
+
+        if arguments.email is not None:
+            user_email = arguments.email
+        else:
+            user_email = sender_email
+
+        if arguments.week is not None:
+            week_number = int(arguments.week)
+        else:
+            week_number = datetime.datetime.now().isocalendar()[1]
+            if week_number == 1:
+                send_message(
+                    space_name = space_name,
+                    message = 'This is the first week of the year. Please wait for data to accumulate. I currently cannot see previous years'
+                )
+                return
+            else:
+                week_number -= 1
+
+        if command_id == 1:
+            breakdown_for_user(user_email, week_number, space_name)
+        else:
+            send_message(
+                space_name = space_name,
+                message = 'I do not recognize that command. Try using the /weeklySummary command.'
+            )
+            return
+
+    # check if the event was a message
+    elif event_type == 'MESSAGE':
+        message = 'You said: `' + event['message']['argumentText'] + '`\n'
+        message += 'Your email address is ' + sender_email + '.\n'
+        message += 'Try using the /weeklySummary command!'
+
+        send_message(
+            space_name = space_name,
+            message = message
+        )
+        return
+
 
 
 if __name__ == '__main__':
